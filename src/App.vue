d94ab42c3ae055183c3d3fc117bb318d4ab6745a
xinshaoguang <58464732+xinshaoguang@users.noreply.github.com>
2020-03-20 15:04:03 +0800

    添加头部、内容部分，底部三个盒子

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+     <v-header></v-header>
+    <v-content></v-content>
+    <v-footer></v-footer>
   </div>
 </template>
 
 <script>
+// const vHeader = () => import("./components/header/header");
+// const vContent = () => import("./components/content/content");
+// const vFooter = () => import("./components/footer/footer");
+import  vHeader from  "./components/header/header"
+import  vContent from  "./components/content/content"
+import  vFooter from  "./components/footer/footer"
 export default {
-  name: 'App'
+   components: {
+    vHeader,
+    vContent,
+    vFooter,
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/components/footer/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>789</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
+
+<style>
+
+</style>
