ad2d567a6883b80170dfac35e297e07aa5826830
xinshaoguang <58464732+xinshaoguang@users.noreply.github.com>
2020-03-20 16:26:44 +0800

    安装并配置elementUi

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 import $ from 'jquery'
 import 'bootstrap3/dist/css/bootstrap.min.css'
